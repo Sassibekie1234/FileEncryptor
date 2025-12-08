8e72b5f31077f161be23c3fbd4545f77aa5c61a9
Sassibekie1234 <167786436+Sassibekie1234@users.noreply.github.com>
2025-12-08 14:37:10 +0000

    Making the main parts of the program

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import getpass
+
 '''
 This is a file encryption tool using the Vigenère cipher. 
 This code is written by Sassibekie1234.
@@ -13,8 +15,12 @@ print("Hello, World!")
 print("This is beeing worked on")
 
 # load the file into memory
+file = open("testfile.txt", "r")
+file.close()
 # ask wether they want to encrypt or decrypt
+action = input("Do you wanna encrpyt of decrypt? ")
 # ask for the key/password
+key = getpass.getpass(prompt = "Enter your key:")
 # make a new file containing the encrypted/decrypted text
 
 

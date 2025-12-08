97c973985b0e56baeda4e85e6020fa59ade5be85
Sassibekie1234 <167786436+Sassibekie1234@users.noreply.github.com>
2025-12-08 14:26:55 +0000

    Making a testfile.txt and laying the program out in pseudocode

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ This is a file encryption tool using the Vigenère cipher.
 This code is written by Sassibekie1234.
 '''
 
+def encrypt(text):
+    print("Still working on this")
+
+def decrypt(text):
+    print("Still working on this")
+
 print("Hello, World!")
-print("This is not finished")
+print("This is beeing worked on")
+
+# load the file into memory
+# ask wether they want to encrypt or decrypt
+# ask for the key/password
+# make a new file containing the encrypted/decrypted text
+
+

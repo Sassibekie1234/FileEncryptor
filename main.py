d9f58db935100f8dc837ea5c41ae944c374c1be7
Sassibekie1234 <167786436+Sassibekie1234@users.noreply.github.com>
2025-12-10 18:28:05 +0000

    changing the filehandling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ This code is written by Sassibekie1234.
 '''
 
 def encrypt(text):
+    lines = len(text)
     print("Still working on this")
 
 def decrypt(text):
@@ -14,9 +15,12 @@ def decrypt(text):
 print("Hello, World!")
 print("This is beeing worked on")
 
-# load the file into memory
-file = open("testfile.txt", "r")
-file.close()
+filename = input("What file do you wanna encrypt?")
+with open("geek.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+
 # ask wether they want to encrypt or decrypt
 action = input("Do you wanna encrpyt of decrypt? ")
 # ask for the key/password
